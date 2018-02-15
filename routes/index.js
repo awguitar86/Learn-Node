1c6982ff9c367fba93f73130c26b97c7a83da613
Austin Wright <awguitar86@gmail.com>
2018-02-15 15:53:20 -0700

    wrote some examples of connecting pug template files to index.js for rendering

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  // const wes = { name: 'Wes', age: 100, cool: true};
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true};
+  // res.send('Hey! It works!');
   // res.json(wes);
   // res.send(req.query);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'wes',
+    dog: req.query.dog,
+    title: 'I love food'
+  });
 });
 
 // router.get('/reverse/:name', (req, res) => {
