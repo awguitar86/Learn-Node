8c36989c6d35f7f71c26a2c18c111ed8a66e197b
Austin Wright <awguitar86@gmail.com>
2018-02-15 14:42:16 -0700

    wrote different ways to send data to browser, and wrote notes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  // const wes = { name: 'Wes', age: 100, cool: true};
   res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query);
 });
 
+// router.get('/reverse/:name', (req, res) => {
+//   const reverse = [...req.params.name].reverse().join('');
+//   res.send(reverse);
+// });
+
 module.exports = router;
+
+
+//req: has all of the information
+//res: has all the methods for sending data back
+//res.query: to get query params
+//res.params: used to access the things that are in the url
